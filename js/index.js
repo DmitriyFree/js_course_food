0412574d05471a36d5a48a246f54c56f379d1d9b
Dmitriy <dmitriyolv@gmail.com>
2021-07-29 15:14:45 +0300

    03

--- js/index.js
+++ js/index.js
@@ -94,4 +94,46 @@ window.addEventListener('DOMContentLoaded', () => {
 
   setClock('.timer', deadline);
 
+  // Modal
+
+  const modalTriger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalClose = document.querySelector('[data-close]');
+
+
+  function showModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = "hidden";
+  }
+
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = "";
+  }
+
+  modalTriger.forEach(item => {
+    item.addEventListener('click', () => {
+      showModal();
+    });
+  });
+
+  modalClose.addEventListener('click', () => {
+    closeModal();
+  });
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+
 });
